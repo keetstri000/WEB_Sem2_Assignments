bdb73b96037c0195707e25d9a3399fb1915619c6
Tristan Keets <keetstri000@warren.k12.in.us>
2024-01-11 08:58:05 -0500

    3

---
+++ Unit 01_JavaScript_Basics and DOM Manipulation/04_Operators, Assignment, and Arithmetic/script.js
@@ -0,0 +1,15 @@
+let number1 = prompt("Enter a number: ");
+let number2 = prompt("Enter another number: ");
+
+number1 = +number1;
+number2 = +number2;
+
+console.log(typeof(number1))
+
+sum = number1 + number2;
+dif = number1 + number2;
+prod = number1 + number2;
+quo = number1 + number2;
+mod = number1 + number2;
+
+document.getElementById("sum").innerHTML = sum;
