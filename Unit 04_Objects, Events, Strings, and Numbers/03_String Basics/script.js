afc4983a3f159a078b41c528286d6c06b0835e64
Tristan Keets <keetstri000@warren.k12.in.us>
2024-02-05 08:32:44 -0500

    lyrics

---
+++ Unit 04_Objects, Events, Strings, and Numbers/03_String Basics/script.js
@@ -0,0 +1,3 @@
+let lyrics = '[Award Tour] We on a world tour with Muhammad, my man. Going each and every place with the mic in their hand Oaktown, LA, San Fran, St John.';
+
+document.getElementById("lyric").innerHTML = lyrics;
