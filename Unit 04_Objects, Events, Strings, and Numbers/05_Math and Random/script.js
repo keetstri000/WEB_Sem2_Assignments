a88d61069995e959347fd89674b3f23efcc69218
Tristan Keets <keetstri000@warren.k12.in.us>
2024-02-06 09:08:08 -0500

    sqrt

---
+++ Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
@@ -0,0 +1,7 @@
+function takeSquareRoot() {
+    let number = +document.getElementById("input1").value;
+
+    let squareRoot = Math.sqrt(number);
+    document.getElementById("result").innervalueHTML = squareRoot
+
+}
