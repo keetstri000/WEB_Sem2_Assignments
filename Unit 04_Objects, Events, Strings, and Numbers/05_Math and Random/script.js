2367705d6382680fe30f7e3ec94d94f15ccf8a6b
Tristan Keets <keetstri000@warren.k12.in.us>
2024-02-12 08:33:04 -0500

    45

--- Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
+++ Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
@@ -5,3 +5,15 @@ function takeSquareRoot() {
     document.getElementById("result").innervalueHTML = squareRoot
 
 }
+function takeSin(){
+    let number = +document.getElementById("input2").value;
+
+    let sin = Math.sin(number * Math.PI / 180);
+    document.getElementById("result").innervalueHTML = sin
+}
+function takeCos(){
+    let number = +document.getElementById("input3").value;
+
+    let cos = Math.sin(number * Math.PI / 180);
+    document.getElementById("result").innervalueHTML = cos
+}
